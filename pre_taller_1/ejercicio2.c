45033d8568b3cdf6d525afdafffbffe31e6c4102
Andres-Debugger <76136156+Andres-Debugger@users.noreply.github.com>
2023-03-27 13:28:58 -0400

    [IMP]  Validation of the dates of birth
    Improving the validation of the dates of birth of each patient

--- pre_taller_1/ejercicio2.c
+++ pre_taller_1/ejercicio2.c
@@ -3,7 +3,7 @@
 int main()
 {
     int cant_paciente=0, i=0, edad=0, dia=0, mes=0, año=0, cedula=0, total_mujer=0, total_hombre=0, 
-    total_menores=0, total_mayores=0, dia_actual=1, mes_actual=1, año_actual=0, total_descuent_mujer=0, total_descuent_hombre=0;
+    total_menores=0, total_mayores=0, dia_actual=1, mes_actual=1, año_actual=0, total_descuent_mujer=0, total_descuent_hombre=0, edad_mal=0;
     char sexo;
     
     printf("¿ Cuantos pacientes desea ingresar ?\n");
@@ -91,9 +91,23 @@ int main()
             scanf("%d", &año);
         }
         
-        while((año_actual-año) != edad){
-            printf ("\nSu fecha de naciemiento no coincide con su edad, por favor introdusca el año correcto:\n");
-            scanf("%d", &año);
+        if((año_actual-año) != edad){
+            edad_mal = 1;
+        }else if((dia_actual-dia) < 0){
+            edad_mal = 1;
+        }else if((mes_actual-mes) < 0){
+            edad_mal = 1; 
+        }
+        
+        if(edad_mal = 1){
+            printf ("\nSu fecha de naciemiento no coincide con su edad, por favor reinicie el programa e introduzca el dato correcto.\n");
+            return 0;
+        }
+        
+        if((((cedula%10)%2) == 0)&&(sexo = 'm')){
+            total_descuent_mujer = total_descuent_mujer + 1;
+        }else if(edad > 60){
+            total_descuent_hombre = total_descuent_hombre + 1;
         }
         
         if((((cedula%10)%2) == 0)&&(sexo = 'm')){
