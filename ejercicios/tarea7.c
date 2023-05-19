ca033f5a46f6d518d742e9b4947d0cbca9f7931a
Andres-Debugger <santiagomartingal@gmail.com>
2023-05-19 00:54:51 -0400

    Adding exercices and homeworks

---
+++ ejercicios/tarea7.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main()
+{
+    int numero;
+    
+    printf("Ingrese un numero entero: ");
+    scanf("%d", &numero);
+
+    if(numero % 2 == 0)
+    {
+        printf("El numero ingresado es par\n");
+    }
+    else
+    {
+        printf("El numero ingresado es impar\n");
+    }
+    
+}
